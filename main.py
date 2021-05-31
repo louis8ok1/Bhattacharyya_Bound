58f049bbf50fb2c355a48317c1e2b90352d46a98
Louis <louis8ok1@gmail.com>
2021-05-31 23:25:52 +0800

    finish

--- main.py
+++ main.py
@@ -29,15 +29,15 @@ def cal_BhattacharyyaDistance(m1,m2,covMatrix1,covMatrix2):
 
     meanDiff = ((1/8) * (array2- array1).T.dot(np.linalg.inv(0.5*(covMatrix1+covMatrix2)))).dot( array2- array1)
     print("mean difference :",meanDiff[0][0])
-    temp  = np.linalg.det(covMatrix1)*np.linalg.det(covMatrix2)
     
+    temp  = np.linalg.det(covMatrix1)*np.linalg.det(covMatrix2)  
     covDiff  = 0.5 * np.log(np.linalg.det((covMatrix1+covMatrix2)*0.5)/(temp**0.5))
     print("covariance difference: ",covDiff)
     ans = meanDiff[0][0]+covDiff
     print("BhattacharyyaDistance: ",ans)
     return ans
     
-def cal_BhattacharyyaErrorBound(p1,p2,s,ans1):
+def cal_BhattacharyyaErrorBound(p1,p2,ans1):
 
     
     bayesError = ((p1*p2)**0.5)*math.exp(-ans1)
@@ -47,18 +47,17 @@ def cal_BhattacharyyaErrorBound(p1,p2,s,ans1):
     
 if __name__ == '__main__':
     m1,var1 = 0,0.9
-    m2,var2 = 2.5,0.7
+    m2,var2 = 0.5,0.7
     p1,p2 = 0.5,0.5
     s = 0.5
     #CovMatrix
     covMatrix1 = cal_CovarianceMatrix(var1)
     covMatrix2 = cal_CovarianceMatrix(var2)
-    print(covMatrix1)
-    print(covMatrix2)
+
     
-    
-    #Carculate Bhawttacharyya Bound
+    #Calculate Bhawttacharyya Distance
     ans = cal_BhattacharyyaDistance(m1,m2,covMatrix1,covMatrix2)
-    #Carculate Bhattacharyya error bound
-    cal_BhattacharyyaErrorBound(p1,p2,s,ans)
+    #Calculate Bhattacharyya error bound
+    cal_BhattacharyyaErrorBound(p1,p2,ans)
+    
  
